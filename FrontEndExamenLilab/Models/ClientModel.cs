248a5e8f3edb80a4e34b0030f0d38d68bd9abf20
Jose Cristobal <jcristobal@hplperu.com>
2020-07-25 18:07:43 -0500

    commit final

--- FrontEndExamenLilab/Models/ClientModel.cs
+++ FrontEndExamenLilab/Models/ClientModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,12 @@ namespace FrontEndExamenLilab.Models
     public class ClientModel
     {
         public int id { get; set; }
+        [Required]
         public string firstName { get; set; }
+        [Required]
         public string lastName { get; set; }
         public string email { get; set; }
+        [Required]
         public DateTime? birthdayDate { get; set; }
     }
 }
